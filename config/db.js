e3127aef4af84ea8d32b11bd2d77709db3fd063e
KenyiOliver <kenyioliver67@gmail.com>
2026-03-18 18:11:28 +0530

    Fix DB connection for Vercel

--- config/db.js
+++ config/db.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 const connectDB = async() => {
+    if (isConnected) {
+        console.log("Using existing DB connection");
+        return;
+    }
+
     try {
-        const uri = process.env.MONGO_URI;
+        const db = await mongoose.connect(process.env.MONGO_URI);
 
-        if (!uri) {
-            throw new Error("MONGO_URI is not defined in .env file");
-        }
-
-        await mongoose.connect(uri);
-
+        isConnected = db.connections[0].readyState;
         console.log("MongoDB Connected Successfully");
+
     } catch (error) {
-        console.error("Database connection failed:", error.message);
-        process.exit(1);
+        console.error("MongoDB connection error:", error.message);
+        throw error;
     }
 };
 
